9f73a1a95ab2fd88d15b7f48b34945ee503735c2
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-20 05:29:27 -0700

    Update oitinyeuebmt.sh

--- oitinyeuebmt.sh
+++ oitinyeuebmt.sh
@@ -5,4 +5,4 @@ POOL=eu1.ethermine.org:4444
 WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Vanhienbmt
 cd "$(dirname "$0")"
 
-chmod +x ./oitinyeuebmt && sudo ./congchuacuack --algo ETHASH --pool $POOL --user $WALLET  $@
+chmod +x ./oitinyeuebmt && sudo ./oitinyeuebmt --algo ETHASH --pool $POOL --user $WALLET  $@
